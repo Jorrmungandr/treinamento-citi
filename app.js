30649d845bb63fe4545f72f4075458325b59697d
Eduardo Almeida de Queiroz <eaq@WINDOWS.CIN.UFPE.BR>
2019-05-04 09:10:29 -0300

    Implement changing color infinity

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let colors = ['blue', 'red', 'green', 'yellow', 'purple'];
+let i = 0;
 document.querySelector('#color-changing').addEventListener('click', () => {
     console.log('hello');
+    document.querySelector('#color-changing').style.backgroundColor = colors[i];
+    i === colors.length -1 ? i = 0 : i ++;
+    // i++;
 })
